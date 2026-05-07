b3d9e6134a1c5e4cee44470f05e1dc0820541558
ABNOP-sudo <fox333opka@gmail.com>
2026-05-07 20:57:29 +0200

    Added FOV and Money Exploit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using OneArmedCookCheat;
+using Swed64;
+
+Swed swed = new Swed("OAC-Win64-Shipping");
+Renderer renderer = new Renderer();
+Thread renderThread = new Thread(() => renderer.Start().Wait());
+renderThread.Start();
+
+IntPtr gameBase = swed.GetModuleBase("OAC-Win64-Shipping.exe");
+
+// Pointers
+IntPtr FOVPointer = swed.ReadPointer(gameBase, 0x04C37FA0, 0x0, 0x20, 0x520) + 0x1F8;
+IntPtr MoneyPointer = swed.ReadPointer(gameBase, 0x04C444F0, 0x30, 0x98, 0xDF8) + 0x238;
+
+renderer.IntFOV = swed.ReadFloat(FOVPointer);
+renderer.currentMoney = swed.ReadInt(MoneyPointer);
+
+while (true) {
+    swed.WriteFloat(FOVPointer, renderer.IntFOV);
+    swed.WriteInt(MoneyPointer, renderer.currentMoney);
+    Thread.Sleep(16);
+}
---
+++ Renderer.cs
@@ -0,0 +1,44 @@
+﻿using ClickableTransparentOverlay;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using ImGuiNET;
+
+
+namespace OneArmedCookCheat
+{
+    public class Renderer : Overlay
+    {
+        public float IntFOV;
+        public int currentMoney;
+        protected override void Render()
+        {
+            ImGui.Begin("OAC Trainer");
+            if (ImGui.BeginTabBar("CheatMenu"))
+            {
+                if (ImGui.BeginTabItem("General"))
+                {
+                    ImGui.InputFloat("FOV", ref IntFOV);
+                    ImGui.InputInt("Current Money", ref currentMoney);
+                    ImGui.EndTabItem();
+                }
+
+                if (ImGui.BeginTabItem("Movement"))
+                {
+
+                    ImGui.EndTabItem();
+                }
+
+                if (ImGui.BeginTabItem("Misc"))
+                {
+                    ImGui.EndTabItem();
+                }
+
+                ImGui.EndTabBar();
+            }
+        }
+    }
+}
